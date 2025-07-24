a119a94bed867801a4e04188358cba9ecf33b158
TarunEswar068 <taruneswar.68@gmail.com>
2025-07-24 10:54:04 +0530

    B

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -3,7 +3,7 @@ public class SudokuSolver {
     private static final int GRID_SIZE = 9;
 
     public static void main(String[] args) {
-        int[][] board = {
+        int[][] badu = {
             {7, 0, 2, 0, 5, 0, 6, 0, 0},
             {0, 0, 0, 0, 0, 3, 0, 0, 0},
             {1, 0, 0, 0, 0, 9, 5, 0, 0},
